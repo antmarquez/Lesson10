f75c46725d77923873b3d8b88914a2a1417b938f
antmarquez <aj.marquez@sbcglobal.net>
2020-04-12 21:45:33 -0700

    Determine if word is palindrome or not

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,5 +1,9 @@
 package exercises;
 
+//Anthony Marquez
+
+import examples.FileHelper;
+
 import java.util.List;
 
 import examples.FileHelper;
@@ -8,5 +12,24 @@ public class Palindrome {
 
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+		
+	}
+	
+	public List<String> wordExists() {
+			return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+			
+		}
+		
+	public boolean isPalindrome(String word) {
+			String reverse = new StringBuffer(word).reverse().toString();
+			boolean palindrome;
+			if(word.equalsIgnoreCase(reverse)) {
+				palindrome = true;
+			}
+			else {
+				palindrome = false;
+			}
+			return palindrome;
+			
 	}
 }
